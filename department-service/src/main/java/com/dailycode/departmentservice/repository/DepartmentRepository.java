9b69264ccdae04f20be1770cd6b9548bb20347fb
s.z.abedeen@gmail.com <s.z.abedeen@gmail.com>
2021-01-10 10:13:15 +0530

    Add micro-services

---
+++ department-service/src/main/java/com/dailycode/departmentservice/repository/DepartmentRepository.java
@@ -0,0 +1,13 @@
+package com.dailycode.departmentservice.repository;
+
+import com.dailycode.departmentservice.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    Department findByDepartmentId(Long departmentId);
+
+
+}
---
+++ user-service/src/main/java/com/dailycode/userservice/VO/ResponseTemplateVO.java
@@ -0,0 +1,15 @@
+package com.dailycode.userservice.VO;
+
+import com.dailycode.userservice.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseTemplateVO {
+
+    private User user;
+    private Department department;
+}
